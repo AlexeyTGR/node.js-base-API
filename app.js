d5778972085897c317ab40d9cf80f553b547c300
Alexey Yegorov <ye.alexey@gmail.com>
2022-04-26 19:03:43 +0300

    add sign up/in

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.get('/users/:id', checkToken, async (req, res) => {
   };
 });
 
-app.post('/registration', async (req, res) => {
+app.post('/signup', async (req, res) => {
   if (!req.body) { return res.sendStatus(400) };
 
   const {
@@ -90,7 +90,6 @@ app.post('/registration', async (req, res) => {
     dateOfBirth: dateOfBirth,
   };
 
-
   try {
     const result = await User.create(newUser);
     const token = jwt.sign({ id: result.id }, privateKey);
@@ -102,6 +101,31 @@ app.post('/registration', async (req, res) => {
     console.error('registration error:', error);
     return res.status(400).json({ message: error.message });
   };
+})
+
+app.get('/signin', async (req, res) => {
+  if (!req.body) { return res.sendStatus(400) };
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ where: { email: email } })
+    if (!user) {
+      return res.status(404).json('User with this email not found')
+    }
+    if (user.password === password) {
+
+      const token = jwt.sign({ id: user.id }, privateKey);
+      return res.status(200).json({
+        message: 'You are signed in',
+        user,
+        token
+      });
+    } else {
+      return res.status(401).json('Wrong password')
+    }
+  } catch (error) {
+    console.error('authentication error:', error);
+    return res.status(400).json({ message: error.message });
+  }
 })
 
 app.delete('/delete/:id', checkToken, (req, res) => {
